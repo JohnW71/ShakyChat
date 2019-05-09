8767e442114bceeaceae0ca01662bce9f3fbec09
John Wingfield <john@johnwingfield.com>
2019-05-09 23:05:08 +0200

    Populate listbox from history

--- shakychat.h
+++ shakychat.h
@@ -10,15 +10,26 @@
 #define WINDOW_WIDTH 600
 #define WINDOW_HEIGHT 400
 
-// #define assert(expression) if(!(expression)) {*(int *)0 = 0;}
-// #define arrayCount(array) (sizeof(array) / sizeof((array)[0]))
+#define assert(expression) if(!(expression)) {*(int *)0 = 0;}
+#define arrayCount(array) (sizeof(array) / sizeof((array)[0]))
 
 void clearArray(char *, int);
 void writeFile(char *, char *);
 void writeFileW(char *, wchar_t *);
 void readSettings(char *, HWND);
 void writeSettings(char *, HWND);
+void fillListbox(char *, HWND);
+// void push(struct Node **, char *);
+void append(struct Node **, char *);
+void readHistory(char *);
+void printList(struct Node *);
 
 LRESULT CALLBACK mainWndProc(HWND, UINT, WPARAM, LPARAM);
 LRESULT CALLBACK customListboxProc(HWND, UINT, WPARAM, LPARAM);
 LRESULT CALLBACK customTextProc(HWND, UINT, WPARAM, LPARAM);
+
+struct Node
+{
+	char *text;
+	struct Node *next;
+};
